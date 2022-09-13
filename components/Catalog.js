ee23c21ad5f3b4a6c345b8511d25aa8dc333f89b
shyndick <3gggkostya@gmail.com>
2022-09-13 12:02:42 +0300

    cat

---
+++ components/Catalog.js
@@ -0,0 +1,15 @@
+class Catalog {
+    constructor() {
+        this.create = () => {
+
+        };
+
+        this.init = () => {
+            console.log('catalog')
+        }
+    }
+}
+
+const catalog = new Catalog().init()
+
+export default catalog
